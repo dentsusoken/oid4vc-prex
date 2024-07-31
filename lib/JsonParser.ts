1fcd01485585a8010de2b96d68b2a8958fe06c24
Yasuo Higa <higayasuo@gmail.com>
2024-07-31 23:46:23 +0900

    Change PresentationExchange.jsonParse.decodePresentationSubmission() signature

--- lib/JsonParser.ts
+++ lib/JsonParser.ts
@@ -21,42 +21,13 @@ import { Result } from './kotlin';
 
 export interface JsonParser {
   /**
-   * Tries to parse the given [inputStream] into a [PresentationDefinition].
-   * It is assumed that the [inputStream] corresponds to a Json object that either contains
-   * a Json object under "presentation_definition" key or is the [PresentationDefinition] itself
-   */
-  // decodePresentationDefinition(
-  //   inputStream: InputStream
-  // ): Result<PresentationDefinition>;
-
-  /**
-   * Tries to parse the given [jsonString] into a [PresentationDefinition].
-   * It is assumed that the [jsonString] corresponds to an object that either contains
-   * a Json object under "presentation_definition" key or is the [PresentationDefinition] itself
-   */
-  //   decodePresentationDefinition(
-  //     jsonString: String
-  //   ): Result<PresentationDefinition>;
-
-  /**
-   * Tries to parse the given [inputStream] into a [PresentationSubmission].
-   * It is assumed that the [inputStream] corresponds to a json object that either contains
+   * Tries to parse the given [jsonString]/[inputStream] into a [PresentationSubmission].
+   * It is assumed that the [jsonString]/[inputStream] corresponds to a json object that either contains
    * a Json object under some well known location (embedded locations) or is the [PresentationSubmission]
    *
    * @see <a href="https://identity.foundation/presentation-exchange/spec/v2.0.0/#embed-locations>embed-locations</a>
    */
   decodePresentationSubmission(
-    input: ReadableStream<Uint8Array>
-  ): Promise<Result<PresentationSubmission>>;
-
-  /**
-   * Tries to parse the given [jsonString] into a [PresentationSubmission].
-   * It is assumed that the [jsonString] corresponds to a json object that either contains
-   * a Json object under some well known location (embedded locations) or is the [PresentationSubmission]
-   *
-   * @see <a href="https://identity.foundation/presentation-exchange/spec/v2.0.0/#embed-locations>embed-locations</a>
-   */
-  decodePresentationSubmission(
-    input: string
+    input: string | ReadableStream<Uint8Array>
   ): Promise<Result<PresentationSubmission>>;
 }
