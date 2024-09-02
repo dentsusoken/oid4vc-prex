7a1c7cca0503910a5f0b19a20828a2deb12f1dbb
ryohei-ebisawa <xebisawa.ryohei@contract.isid.co.jp>
2024-09-02 13:41:23 +0900

    refactor: Separate FieldConstraint class into its own file and implement toJSON, fromJSON methods
    - Moved FieldConstraint class from /lib/Types.ts to /lib/types/FieldConstraint.ts
    - Implemented toJSON and fromJSON methods in FieldConstraint class
    - Created fieldConstraintSchema with zod for FieldConstraint class validation

---
+++ lib/types/FieldConstraint.spec.ts
@@ -0,0 +1,117 @@
+import { describe, expect, it } from 'vitest';
+import {
+  pathSchema,
+  fieldConstraintSchema,
+  FieldConstraint,
+} from './FieldConstraint';
+import { Id } from '../value-objects/Id';
+import { Name } from '../value-objects/Name';
+import { Purpose } from '../value-objects/Purpose';
+import { Filter } from './Filter';
+
+describe('FieldConstraint', () => {
+  describe('pathSchema', () => {
+    it('should be a non-empty set of JSON paths', () => {
+      const validPaths = ['$.a', '$.b'];
+      const invalidPaths = ['', '$.a', 123];
+
+      expect(pathSchema.safeParse(validPaths)).toEqual({
+        success: true,
+        data: validPaths,
+      });
+      expect(pathSchema.safeParse(invalidPaths).success).toBe(false);
+    });
+  });
+  describe('fieldConstraintSchema', () => {
+    it('should be a valid FieldConstraint', () => {
+      const validFieldConstraint = {
+        path: ['$.a'],
+        id: '123',
+        name: 'name',
+        purpose: 'purpose',
+        filter: { type: 'string' },
+        optional: true,
+        intent_to_retain: true,
+      };
+      const invalidFieldConstraint = {
+        path: ['.a'],
+        id: '123',
+        name: 'name',
+        purpose: 'purpose',
+        filter: { type: 'string' },
+        optional: true,
+        intent_to_retain: true,
+      };
+
+      expect(fieldConstraintSchema.safeParse(validFieldConstraint)).toEqual({
+        success: true,
+        data: validFieldConstraint,
+      });
+
+      expect(
+        fieldConstraintSchema.safeParse(invalidFieldConstraint).success
+      ).toBe(false);
+    });
+  });
+  describe('constructor', () => {
+    it('should create a new FieldConstraint instance', () => {
+      const fieldConstraint = new FieldConstraint(
+        ['$.a'],
+        new Id('123'),
+        new Name('name'),
+        new Purpose('purpose'),
+        new Filter({ type: 'string' }),
+        true,
+        true
+      );
+      expect(fieldConstraint).toBeInstanceOf(FieldConstraint);
+    });
+  });
+  describe('fromJSON', () => {
+    it('should create a FieldConstraint instance from a JSON object', () => {
+      const fieldConstraint = FieldConstraint.fromJSON({
+        path: ['$.a'],
+        id: '123',
+        name: 'name',
+        purpose: 'purpose',
+        filter: { type: 'string' },
+        optional: true,
+        intent_to_retain: true,
+      });
+      expect(fieldConstraint).toBeInstanceOf(FieldConstraint);
+      expect(fieldConstraint.paths).toStrictEqual(['$.a']);
+      expect(fieldConstraint.id).toBeInstanceOf(Id);
+      expect(fieldConstraint.id?.value).toStrictEqual('123');
+      expect(fieldConstraint.name).toBeInstanceOf(Name);
+      expect(fieldConstraint.name?.value).toStrictEqual('name');
+      expect(fieldConstraint.purpose).toBeInstanceOf(Purpose);
+      expect(fieldConstraint.purpose?.value).toStrictEqual('purpose');
+      expect(fieldConstraint.filter).toBeInstanceOf(Filter);
+      expect(fieldConstraint.filter?.value).toStrictEqual({ type: 'string' });
+      expect(fieldConstraint.optional).toStrictEqual(true);
+      expect(fieldConstraint.intent_to_retain).toStrictEqual(true);
+    });
+  });
+  describe('toJSON', () => {
+    it('should return a JSON representation of the FieldConstraint', () => {
+      const fieldConstraint = new FieldConstraint(
+        ['$.a'],
+        new Id('123'),
+        new Name('name'),
+        new Purpose('purpose'),
+        new Filter({ type: 'string' }),
+        true,
+        true
+      );
+      expect(fieldConstraint.toJSON()).toStrictEqual({
+        path: ['$.a'],
+        id: '123',
+        name: 'name',
+        purpose: 'purpose',
+        filter: { type: 'string' },
+        optional: true,
+        intent_to_retain: true,
+      });
+    });
+  });
+});
