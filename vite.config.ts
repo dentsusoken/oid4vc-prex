6b7478a7395814e4ded469bc5c8722cce255d49f
Yasuo Higa <higayasuo@gmail.com>
2025-07-04 13:01:51 +0900

    chore: Bump version to 0.1.0 and update build configuration
    - Updated version in package.json and package-lock.json to 0.1.0.
    - Modified vite.config.ts to support multiple entry points for library builds.
    - Adjusted output file naming based on entry point in vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,19 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     lib: {
-      entry: './lib/index.ts',
+      entry: {
+        index: './lib/index.ts',
+        internal: './lib/internal/index.ts',
+      },
       name: 'oid4vc-prex',
       formats: ['es', 'cjs'],
-      fileName: (format) => `index.${format === 'es' ? 'mjs' : 'cjs'}`,
+      fileName: (format, entry) => {
+        const ext = format === 'es' ? 'mjs' : format;
+        const indexFile = `index.${ext}`;
+
+        return entry === 'index' ? indexFile : `${entry}/${indexFile}`;
+        //return `${dir}/index.${ext}`;
+      },
     },
     rollupOptions: {
       external: ['@vecrea/oid4vc-core'],
